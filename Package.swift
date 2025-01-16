5a9ef7b3dc5d42641978b7d590ddd624be7ab99d
kailen <1457877626@qq.com>
2025-01-16 15:36:04 +0800

    feat:更新模拟器framework

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/StarSkyK/openvpn-ios/releases/download/v6.4/openssl.xcframework.zip",
-            checksum: "a6b9f2deaaf92717a6b0798423c1f3808f59a69cb92c1842c54dec5f056362d1"),
+            url: "https://github.com/StarSkyK/openvpn-ios/releases/tag/v6.4.1/openssl.xcframework.zip",
+            checksum: "1fb9c5aabb7bf221390d7ac32f5fea1b0868e61ee525a364c3b1b29b5f8019d3"),
 
         // local development
 //        .binaryTarget(
